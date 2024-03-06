7761282cf53a7140686cf23b3e96df317b8dc436
Mohamed KADI <handskadi@gmail.com>
2024-03-06 17:53:58 +0100

    Task: continue... 3

--- practice/mk-workshop.js
+++ practice/mk-workshop.js
@@ -30,18 +30,56 @@ export function mkTask() {
       // return 'It\'s another day.';
   }
 
-  // Functions: 
+  // Declarition Functions
   function add(x,y){
     return x + y;
   }
   // return add(5,4);
 
+  // Function expression
   let sub = function(x, y){
     return x - y;
   }
-  //return sub(10,3);
 
+  // Arroe Functions:
+  let multiply = (a, b) => a * b;
+  let name = (name) => "Myname is " + name;
 
+  // return name("Mohamed KADI!");
+
+  // Higher-order function
+  function processNumbers(numbers, callback) {
+    for (let i = 0; i < numbers.length; i++) {
+        // Call the callback function for each number in the array
+        callback(numbers[i]);
+    }
+  }
+
+  // Callback function to double each number
+  function doubleNumber(number) {
+    console.log(number * 2);
+  }
+
+  // Array of numbers
+  const numbers = [1, 2, 3, 4, 5];
+  // accessing and modifying array elements:
+  numbers.push(6);
+  let firstelement = numbers[4]
+
+  //return firstelement;
+
+  // Array methods
+  numbers.push(9);
+  numbers.forEach(num => console.log(num));
+  
+  // Object: TODO
+
+  // Process numbers with callback function
+  // console.log("Doubling each number:");
+  // processNumbers(numbers, doubleNumber);
+  
+  //return multiply(3,5)
+  // return sub(10,3);
 
   // return result;
   // return object[Object.keys(object)[0]]; //value
